9d8f25e1b2cff716c30d64e773877194305d811e
Tsoympet <tsoympet7@gmail.com>
2025-05-30 21:44:32 +0300

    Update helpers.py

--- src/extension/utils/helpers.py
+++ src/extension/utils/helpers.py
@@ -1,1 +1,17 @@
+from cryptography.fernet import Fernet
+import logging
 
+def setup_logging():
+    logging.basicConfig(
+        level=logging.INFO,
+        filename="logs/gaia.log",
+        format="%(asctime)s - %(levelname)s - %(message)s"
+    )
+
+def encrypt_data(data, key):
+    cipher = Fernet(key)
+    return cipher.encrypt(data.encode())
+
+def decrypt_data(encrypted_data, key):
+    cipher = Fernet(key)
+    return cipher.decrypt(encrypted_data).decode()
